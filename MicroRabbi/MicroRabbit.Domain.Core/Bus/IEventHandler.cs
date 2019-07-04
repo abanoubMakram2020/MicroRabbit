6fb94ca2e33fc176c30e05eb5a6529f6f5542d4c
Angular_Demo <hegab@github.com>
2019-07-04 16:11:05 +0200

    implemented interfaces and classes to Domain.Core

---
+++ MicroRabbi/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent>: IEventHandler
+        where TEvent:Event
+    {
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
