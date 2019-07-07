747403a1a1dc95365c197fe62c8d5a7fa1271d56
Angular_Demo <hegab@github.com>
2019-07-07 17:47:45 +0200

    partialy completed banking microservice

---
+++ MicroRabbi/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepositry _accountRepository;
+
+        public AccountService(IAccountRepositry accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public IEnumerable<Account> GetAccouns()
+        {
+            return _accountRepository.GetAccounts();
+        }
+    }
+}
--- MicroRabbi/MicroRabbit.Infra.Ioc/DependencyContainer.cs
+++ MicroRabbi/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,6 +18,14 @@ namespace MicroRabbit.Infra.Ioc
         {
             //Domain Bus
             service.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Service
+            service.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            service.AddTransient<IAccountRepositry, AccountRepository>();
+            service.AddTransient<BankingDbContext>();
+          
         }
     }
 }
